0ed263ce437b96921314d70f6465e76cff1065e8
10groups <67424868+10Groups@users.noreply.github.com>
2020-07-21 22:09:34 +0800

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,9 +1,10 @@
 ideologies = [
+//Centrist
     {
         "name": "Centrism",
 	    	"link": "https://en.wikipedia.org/wiki/Centrism",
 		"desc": "Centrism is a political position that supports a balance between left-leaning and right-leaning policies. It opposes a significant shift in society.",
-		"next": "Radical Centrism, Apoliticism, Neoliberalism",
+		"next": "Radical Centrism, Apoliticism, Neoliberalism, Center-Left Politics, Center-Right Politics",
         "stats": {
 	  "econ": 0,
 	  "govt": 0,
@@ -23,15 +24,72 @@ ideologies = [
           "cltrassi": 0,
           "proccomp": 0,
           "proctrans": 0,
-          "polapol": 0,
-          "polext": 0
+          "polapol": -5,
+          "polext": -5
         }
     },
+	{
+        "name": "Radical Centrism",
+	    	"link": "https://en.wikipedia.org/wiki/Radical_centrism",
+		"desc": "Radical Centrism is a political ideology that aims to borrow ideas from different sides, often merging them toghether to create an independent worldview.",
+		"next": "Centrism, Apoliticism, Neoliberalism, Center-Left Politics, Center-Right Politics",
+        "stats": {
+	  "econ": 0,
+	  "govt": 2,
+	  "econsys": 0,
+	  "econreg": 0,
+          "govtsize": 2,
+          "govttype": 0,
+          "diplapp": 0,
+          "diplrel": 0,
+          "sctychg": 0,
+          "sctytrad": 0,
+          "techacc": 0,
+          "techbio": 0,
+          "lawtype": 0,
+          "lawrehab": 0,
+          "cltrhrchy": 0,
+          "cltrassi": 0,
+          "proccomp": 0,
+          "proctrans": 0,
+          "polapol": 0,
+          "polext": 8
+        }
+    },
+	{
+        "name": "Apoliticism",
+	    	"link": "https://en.wikipedia.org/wiki/Apoliticism",
+		"desc": "Apoliticism is a apathy/antipathy towards most political views. Apoliticals are often neutral in most political views.",
+		"next": "Centrism, Radical Centrism, Neoliberalism, Center-Left Politics, Center-Right Politics",
+        "stats": {
+	  "econ": 0,
+	  "govt": 2,
+	  "econsys": 0,
+	  "econreg": 0,
+          "govtsize": 2,
+          "govttype": 0,
+          "diplapp": 0,
+          "diplrel": 0,
+          "sctychg": 0,
+          "sctytrad": 0,
+          "techacc": 0,
+          "techbio": 0,
+          "lawtype": 0,
+          "lawrehab": 0,
+          "cltrhrchy": 0,
+          "cltrassi": 0,
+          "proccomp": 0,
+          "proctrans": 0,
+          "polapol": -10,
+          "polext": -10
+        }
+    },
+//LibRight
     {
         "name": "Anarcho-Capitalism",
 	    	"link": "https://en.wikipedia.org/wiki/Anarcho-capitalism",
 		"desc": "Anarcho-capitalism is a political ideology that advocates the elimination of the states in favor of private property and free markets. They believe any action should be legal as long as no individual is a victim of agression.",
-		"next": "Hoppeanism, Pink Capitalism, Minarchism",
+		"next": "Hoppeanism, Pink Capitalism, Minarchism, Anarcho-Individualism",
         "stats": {
 	  "econ": 9,
 	  "govt": -9,
