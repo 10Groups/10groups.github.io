3be50a233793e03e14d1b3f73decd319b9ce9d3b
10groups <67424868+10Groups@users.noreply.github.com>
2020-07-22 11:22:59 +0800

    Create questionstest.js

---
+++ questionstest.js
@@ -0,0 +1,23 @@
+questions =  [
+
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"econ": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"govt": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"econsys": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"econreg": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"govtsize": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"govttype": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"diplapp": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"diplrel": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"sctychg": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"sctytrad": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"techacc": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"techbio": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"lawtype": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"lawrehab": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"cltrhrchy": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"cltrassi": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"proccomp": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"proctrans": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"polapol": 1}},
+{"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"polext": 1}},
+];
